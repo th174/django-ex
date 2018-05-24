8a490c955016f660453c86d368fdaab17262efa6
th174 <th174@duke.edu>
2018-05-24 07:30:59 -0400

    Added Hello World to urls

--- project/urls.py
+++ project/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello_world', hello_world),
 ]
 
 if settings.DEBUG:
